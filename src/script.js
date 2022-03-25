2b20a25647eece90bb662faa9b54958722d49a59
Matias <mpintov42@gmail.com>
2022-03-25 18:01:16 -0300

    changed layout plus added svg, and interaction with social media

--- src/script.js
+++ src/script.js
@@ -70,18 +70,18 @@ window.addEventListener('resize', () =>
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 0
 camera.position.y = 0
-camera.position.z = 5
+camera.position.z = 3
 scene.add(camera)
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
 
 /**
  * Points with plane
  */
 
-const planeGeometry = new THREE.PlaneGeometry(25,25,64,64)
+const planeGeometry = new THREE.PlaneGeometry(100,20,64,64)
 const planeMaterial = new THREE.ShaderMaterial({
     wireframe:true,
     vertexShader: planeVertexShader,
@@ -92,8 +92,10 @@ const planeMaterial = new THREE.ShaderMaterial({
     }
 })
 const plane = new THREE.Mesh(planeGeometry,planeMaterial)
-plane.position.x = 6
-plane.rotation.y = -Math.PI*0.25
+plane.position.x = 0
+plane.position.y = -8.5
+plane.position.z = -10
+plane.rotation.x = -Math.PI * 0.4
 scene.add(plane)
 
 /**
@@ -110,19 +112,19 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Post processing
  */
-const effectComposer = new EffectComposer(renderer)
-effectComposer.setSize(sizes.width, sizes.height)
-effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+// const effectComposer = new EffectComposer(renderer)
+// effectComposer.setSize(sizes.width, sizes.height)
+// effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-const renderPass = new RenderPass(scene, camera)
-effectComposer.addPass(renderPass)
-// const rgbShiftPass = new ShaderPass(RGBShiftShader)
-// effectComposer.addPass(rgbShiftPass)
-// const gammaCorrectionPass = new ShaderPass(GammaCorrectionShader)
-// effectComposer.addPass(gammaCorrectionPass)
-const unrealBloomPass = new UnrealBloomPass()
-unrealBloomPass.strength = 1.5
-effectComposer.addPass(unrealBloomPass)
+// const renderPass = new RenderPass(scene, camera)
+// effectComposer.addPass(renderPass)
+// // const rgbShiftPass = new ShaderPass(RGBShiftShader)
+// // effectComposer.addPass(rgbShiftPass)
+// // const gammaCorrectionPass = new ShaderPass(GammaCorrectionShader)
+// // effectComposer.addPass(gammaCorrectionPass)
+// const unrealBloomPass = new UnrealBloomPass()
+// unrealBloomPass.strength = 1.5
+// effectComposer.addPass(unrealBloomPass)
 
 /**
  * Animate
@@ -179,46 +181,45 @@ const tick = () =>
     // update uTime
     planeMaterial.uniforms.uTime.value = elapsedTime
 
-    planeMaterial.uniforms.uInterp= lerp(0, 1.0,interp)
-    // audio
-    if (playButton.dataset.playing === 'true') {
-        interp+=0.001
-        analyser.getFloatFrequencyData(dataArray)
-        var lowerHalfArray = dataArray.slice(0, (dataArray.length / 2) - 1);
-        var upperHalfArray = dataArray.slice((dataArray.length / 2) - 1, dataArray.length - 1);
+    // // audio
+    // if (playButton.dataset.playing === 'true') {
+    //     interp+=0.001
+    //     analyser.getFloatFrequencyData(dataArray)
+    //     var lowerHalfArray = dataArray.slice(0, (dataArray.length / 2) - 1);
+    //     var upperHalfArray = dataArray.slice((dataArray.length / 2) - 1, dataArray.length - 1);
 
-        var overallAvg = avg(dataArray);
-        var lowerMax = max(lowerHalfArray);
-        var lowerAvg = avg(lowerHalfArray);
-        var upperMax = max(upperHalfArray);
-        var upperAvg = avg(upperHalfArray);
+    //     var overallAvg = avg(dataArray);
+    //     var lowerMax = max(lowerHalfArray);
+    //     var lowerAvg = avg(lowerHalfArray);
+    //     var upperMax = max(upperHalfArray);
+    //     var upperAvg = avg(upperHalfArray);
 
         
 
-        var lowerMaxFr = lowerMax / lowerHalfArray.length;
-        var lowerAvgFr = lowerAvg / lowerHalfArray.length;
-        var upperMaxFr = upperMax / upperHalfArray.length;
-        var upperAvgFr = upperAvg / upperHalfArray.length;
+    //     var lowerMaxFr = lowerMax / lowerHalfArray.length;
+    //     var lowerAvgFr = lowerAvg / lowerHalfArray.length;
+    //     var upperMaxFr = upperMax / upperHalfArray.length;
+    //     var upperAvgFr = upperAvg / upperHalfArray.length;
 
 
-        var scalingRate = Math.tanh(lowerAvgFr) * 4 + 1;
-        plane.scale.set(1+(scalingRate*0.05),1+(scalingRate*0.05));
+    //     var scalingRate = Math.tanh(lowerAvgFr) * 4 + 1;
+    //     plane.scale.set(1+(scalingRate*0.05),1+(scalingRate*0.05));
 
-        const count = planeGeometry.attributes.position.count
-        const randoms = new Float32Array(count)
-        randoms.fill(modulate(upperAvgFr, 0, 1, 0, 4)*0.5)
-        const randoms2 = new Float32Array(count)
-        randoms2.fill(lowerMaxFr)
-        planeGeometry.setAttribute('aBass', new THREE.BufferAttribute(randoms2,1))
-        planeGeometry.setAttribute('aTreble', new THREE.BufferAttribute(randoms,1))
-    }
+    //     const count = planeGeometry.attributes.position.count
+    //     const randoms = new Float32Array(count)
+    //     randoms.fill(modulate(upperAvgFr, 0, 1, 0, 4)*0.5)
+    //     const randoms2 = new Float32Array(count)
+    //     randoms2.fill(lowerMaxFr)
+    //     planeGeometry.setAttribute('aBass', new THREE.BufferAttribute(randoms2,1))
+    //     planeGeometry.setAttribute('aTreble', new THREE.BufferAttribute(randoms,1))
+    // }
 
     // Update controls
-    controls.update()
+    // controls.update()
 
     // Render
-    // renderer.render(scene, camera)
-    effectComposer.render()
+    renderer.render(scene, camera)
+    // effectComposer.render()
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
